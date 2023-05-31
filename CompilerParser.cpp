a4037d22e08ee7a5bdc0b5d1431a28157cbb7228
Mihir Desai <mihirdesai@Mihirs-MacBook-Air.local>
2023-06-01 00:36:21 +0930

    commit

--- CompilerParser.cpp
+++ CompilerParser.cpp
@@ -49,7 +49,7 @@ if(mustBe("keyword","class")){
     ParseTree *m=new ParseTree("keyword","class");
     classt->addChild(m);
 }
-f(mustBe("identifier","Main")){
+if(mustBe("identifier","Main")){
     ParseTree *m=new ParseTree("identifier","Main");
     classt->addChild(m);
 }
@@ -64,7 +64,7 @@ if(mustBe("symbol","}")){
     ParseTree *m=new ParseTree("symbol","}");
     classt->addChild(m);
 }
-return classt;i
+return classt;
 
 }
 
